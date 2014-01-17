68edb423b864ce3eb8ef63d404d9b1b48b31af76
chuaigm <chuai1609@163.com>
2014-01-17 11:30:07 +0800

    svn export diff tool usefull

---
+++ shellscript/svnexportdiff.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ $# -eq 0 ] ; then
+	echo "Usage like: $0 [old_version] [new_version]"
+	exit 1
+fi
+
+if [ $1 -gt $2 ] ; then
+	echo "Usage like: $0 [old_version] [new_version]"
+	exit 1
+fi
+
+OLD_VERSION=$1
+NEW_VERSION=$2
+
+# path for patch files directory
+WORK_PATH=`pwd`
+# local svn directory ready for export(modify here)
+SVN_PATH=/home/chuai/trunk
+
+cd ${SVN_PATH}
+
+echo "start update ......"
+svn up
+echo "end update"
+
+SVN_URL=`svn info | grep "URL" | awk 'NR==1' | awk -F " " '{print $2}'`
+
+echo "start version diff"
+DIFF_NUM=`svn diff -r ${OLD_VERSION}:${NEW_VERSION} . | grep "Index:*" | awk -F " " '{print $2}' | wc -l`
+
+if [ ${DIFF_NUM} -ne 0 ]; then
+	DIFF_LIST=`svn diff -r ${OLD_VERSION}:${NEW_VERSION} . | grep "Index:*" | awk -F " " '{print $2}'`
+	for file in ${DIFF_LIST} ; do
+		echo ${file}
+		FILE_NAME=`basename ${file}`
+		FOLDER_NAME=`dirname ${file}`
+		if test ! -e "${WORK_PATH}/version_patch/${OLD_VERSION}_${NEW_VERSION}/${FOLDER_NAME}"; then
+			mkdir -p ${WORK_PATH}/version_patch/${OLD_VERSION}_${NEW_VERSION}/${FOLDER_NAME}
+		fi
+		cd ${WORK_PATH}/version_patch/${OLD_VERSION}_${NEW_VERSION}/${FOLDER_NAME}/
+		svn export -r ${NEW_VERSION} ${SVN_URL}/${file}
+		cd ${WORK_PATH}
+	done
+	echo "OK"
+else
+	echo "nodiff"
+fi
