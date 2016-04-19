232d509ddcecc2fdf5923853a455e40a01547772
chuaigm <chuai1609@163.com>
2016-04-19 17:20:13 +0800

    bat modify txt tool

---
+++ shellscript/ChangeContent.bat
@@ -0,0 +1,41 @@
+@echo off&setlocal ENABLEDELAYEDEXPANSION
+call :解析 123.txt
+call :提取 conf1 aaa
+call :修改 conf1 aaa 192.168.2.2
+call :输出 >new.ini
+goto :eof
+ 
+ 
+:解析 [参数#1=ini文件路径]
+set "op="
+for /f " usebackq tokens=1* delims==" %%a in ("%~1") do (
+    if "%%b"=="" (
+        set "op=%%a"
+    ) else (
+        set "##!op!#%%a=%%b"
+    )
+)
+goto :eof
+ 
+ 
+:提取 [参数#1=Option] [参数#2=Key]
+echo,Option=%~1,Key=%~2,Value=!##[%~1]#%~2!
+goto :eof
+ 
+ 
+:修改 [参数#1=Option] [参数#2=Key] [参数#3=value，没有参数3则删除该配置项]
+set "##[%~1]#%~2=%~3"
+goto :eof
+ 
+ 
+:输出 [>新ini文件路径]
+set "op="
+for /f "tokens=1-3 delims=#=" %%a in ('set ##') do (
+    if "%%a"=="!op!" (
+        echo,%%b=%%c
+    ) else (
+        echo,%%a
+        set "op=%%a"
+        echo,%%b=%%c
+    )
+)
